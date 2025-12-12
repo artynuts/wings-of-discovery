0e92f7bd766435a95d82b0495585005185a7cd7f
artynuts <aarthi-ntrjn@users.noreply.github.com>
2025-12-12 00:05:43 -0800

    Make Meet the Team tile flow naturally in grid after Kids & Archaeology

--- tello/tello_fly_tilt.py
+++ tello/tello_fly_tilt.py
@@ -10,6 +10,8 @@ print("Battery:", tello.get_battery())
 tello.takeoff()
 tello.move_down(30)
 
+tello.take_picture()
+
 tello.streamon()
 frame = tello.get_frame_read().frame
 cv2.imwrite("tello_photo.jpg", frame)
@@ -20,6 +22,7 @@ time.sleep(1)
 tello.send_rc_control(0,0,0,0)
 
 tello.land()
+
 
 tello.end()
 
